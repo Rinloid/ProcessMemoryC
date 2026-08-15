497a511500c1266b45141bce94cc46f455829bb8
Rin <81559497+Rinloid@users.noreply.github.com>
2026-08-15 23:09:29 +0900

    Add files via upload

---
+++ Example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "ProcessMemory.h"
+
+int main() {
+    ProcessMemory pm;
+	ProcessMemory_Init(&pm, "example.exe", false);
+
+    int32_t address = 0x123456;
+    int32_t offsets[] = {0x123, 0x456, 0x789};
+    int32_t value = 0;
+
+    /* Memory Reading */
+    ProcessMemory_ReadInt32(&pm, address, &value);
+    printf("%d", value);
+
+    /* Memory Writing */
+    ProcessMemory_WriteInt32(&pm, address, 1234);
+
+    /* Pointer Chain Traversal */
+    ProcessMemory_TraverseInt32(&pm, address, offsets, ARRAYSIZE(offsets), &value);
+    printf("%d", value);
+
+    return 0;
+}
