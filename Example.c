a29d910db609ff84e501a26d7894b07472d205f8
Rin <81559497+Rinloid@users.noreply.github.com>
2026-08-15 23:19:13 +0900

    Update Example.c

--- Example.c
+++ Example.c
@@ -5,20 +5,27 @@ int main() {
     ProcessMemory pm;
 	ProcessMemory_Init(&pm, "example.exe", false);
 
+    if (!ProcessMemory_Open(&pm)) {
+		printf("Process Not Found!\n");
+		return 0;
+	}
+
     int32_t address = 0x123456;
     int32_t offsets[] = {0x123, 0x456, 0x789};
     int32_t value = 0;
 
     /* Memory Reading */
     ProcessMemory_ReadInt32(&pm, address, &value);
-    printf("%d", value);
+    printf("%d\n", value);
 
     /* Memory Writing */
     ProcessMemory_WriteInt32(&pm, address, 1234);
 
     /* Pointer Chain Traversal */
     ProcessMemory_TraverseInt32(&pm, ProcessMemory_GetBaseAddress(&pm) + address, offsets, ARRAYSIZE(offsets), &value);
-    printf("%d", value);
+    printf("%d\n", value);
+
+    ProcessMemory_Close(&pm);
 
     return 0;
 }
